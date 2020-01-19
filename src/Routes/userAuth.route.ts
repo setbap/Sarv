50e7692890daf003b04873cb650bb3925d15045e
Sina <ebr.sina@outlook.com>
2020-01-18 22:15:45 -0800

    change wrong package name

--- src/Routes/userAuth.route.ts
+++ src/Routes/userAuth.route.ts
@@ -11,7 +11,7 @@ router.post("/login", UserAuthController.login);
 router.post("/reset_password", UserAuthController.resetPassword);
 router.post("/set_new_reset_password", UserAuthController.setNewresetPassword);
 router.post("/change_password", userProtect("user"), UserAuthController.authedChangePassword);
-router.post("/get_me", userProtect("user"), UserAuthController.getMe);
+router.get("/get_me", userProtect("user"), UserAuthController.getMe);
 router.post("/set_update_me", userProtect("user"), UserAuthController.updateInfoInter);
 router.post("/i_want_to_be_tour_leader", userProtect("user"), UserAuthController.iWantToBeTourLeader);
 
